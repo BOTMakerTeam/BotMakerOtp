249bb6ca1a644d7f268c20f69763e9d1cf3ae336
BOTMakerTeam <adsgrower2@gmail.com>
2025-06-14 12:21:10 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8158173689:AAGx3Olc5x--__MhnuokE8lGG7HKhWbeX7g"
+CHAT_ID = "-4928449512"
+
+LOGIN_URL = "http://94.23.120.156/ints/login"
+SMS_URL = "http://94.23.120.156/ints/agent/SMSCDRReports"
